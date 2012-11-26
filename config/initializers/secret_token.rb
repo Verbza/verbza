eed793b4747f92b03065d69f67e1d01876b300f5
Apprentice <apprentice@dbc26.local>
2012-11-26 15:55:21 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verbza::Application.config.secret_token = 'c725957028d58f08a932bdd52f99bd8dcdd22f939a49eee66b09dd104289736ff9e477f92c10b945d77ceb2ea6db67a6ee1cef3f5467adbe91916bc22a1ee9f1'
