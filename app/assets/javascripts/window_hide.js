219b7d2260dcc728e5c9b47d2b64caf13e609605
Apprentice <apprentice@dbc26.local>
2012-11-27 11:15:58 -0800

    getting url-bar hiding upon page load

---
+++ app/assets/javascripts/window_hide.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  window.addEventListener("load",function() {
+  	// Set a timeout...
+	  setTimeout(function(){
+	    // Hide the address bar!
+	    window.scrollTo(0, 1);
+	  }, 0); 
+  });
+});
