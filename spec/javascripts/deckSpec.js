b90a19555d21d2fafb565ffb9db1f7cc68edd07e
Mike Riess <mikeriess@gmail.com>
2012-12-02 13:44:36 -0800

    Basic quiz functionality

--- spec/javascripts/deckSpec.js
+++ spec/javascripts/deckSpec.js
@@ -2,32 +2,58 @@ describe("Deck", function() {
 	beforeEach(function() {
 		loadFixtures("quiz.html.erb");
 		deck = new Deck();
-		deck.loadCards([{"created_at":"2012-12-01T23:18:17Z",
-						 "foreign_word":"hello",
-						 "id":2,
-						 "native_word":"marhabaan",
-						 "updated_at":"2012-12-01T23:18:17Z",
-						 "user_id":1}]);
+		deck.loadCards([{"native_word":"hello",
+						 "foreign_word":"marhabaan"},
+						{
+						 "native_word":"student",
+						 "foreign_word":"thalib"}]);
 	});
 	
 	it("creates cards with the correct values", function() {
-		expect(deck.cards[0].foreign_word).toEqual("hello");
+		expect(deck.cards[0].foreign_word).toEqual("marhabaan");
 	});
 	
 	describe("quiz", function() {
 		describe("on load", function() {
 			it("displays a start game message", function() {
 				expect($("#word_display")).toHaveText("Start game");
-			})
-		})
+			});
+		});
 		
 		describe("first click", function() {
 			it("displays the native word of the first card", function() {
 				$("#word_display").click();
 				expect($("#word_display")).toHaveText(deck.cards[0].native_word);
-			})
-		})
-	})
+			});
+		});
+		
+		describe("second click", function() {
+			it("displays the foreign word of the first card", function() {
+				$("#word_display").click();
+				$("#word_display").click();				
+				expect($("#word_display")).toHaveText(deck.cards[0].foreign_word);
+			});
+		});
+		
+		describe("third click", function() {
+			it("displays the native word of the second card", function() {
+				$("#word_display").click();
+				$("#word_display").click();
+				$("#word_display").click();
+				
+				expect($("#word_display")).toHaveText(deck.cards[1].native_word);
+			});
+		});
+		
+		describe("end of deck", function() {
+			it("displays a 'quiz complete' message", function() {
+				for(var i = 0; i <= (deck.cards.length * 2); i++) {
+					$("#word_display").click();
+				}
+				expect($("#word_display")).toHaveText("Quiz complete!");
+			});
+		});
+	});
 	
 	describe("AJAX functions", function() {
 		// tests for actual JSON calls
