33b2cadac057fb8fcb395ef0e598d68758644635
danigro77 <daniela.grossmann@gmail.com>
2012-12-04 11:33:03 -0800

    card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,8 +7,8 @@ class Card < ActiveRecord::Base
   attr_accessible :decks, :foreign_word, :native_word, :user_id, :image_file_name, :image
 
   has_attached_file :image,
-      :styles => { 
-      :medium => "300x300>", 
+      :styles => {
+      :medium => "300x300>",
       :thumb => "100x100>" },
     :storage => :s3,
     :s3_credentials => {
@@ -17,12 +17,18 @@ class Card < ActiveRecord::Base
       :secret_access_key => ENV['AWS_SECRET_KEY']
     },
     :path => "/:style/:id/:filename"
-    
 
-  validates :native_word,
-    :presence => true
 
-  validates :foreign_word,
-    :presence => true
+  validates :native_word,  :presence => true, :if => :foreign_word?
+  validates :foreign_word, :presence => true, :if => :native_word?
+
+  def foreign_word?
+    :foreign_word == nil
+  end
+
+  def native_word?
+    :native_word == nil
+  end
+
 
 end
