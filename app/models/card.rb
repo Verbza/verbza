40a4627523698ac73859fe82e80704df4a3489f1
mhriess <mikeriess@gmail.com>
2012-12-04 17:58:11 -0800

    Set up AWS for development environment.

--- app/models/card.rb
+++ app/models/card.rb
@@ -9,12 +9,6 @@ class Card < ActiveRecord::Base
       :styles => {
       :medium => "300x300>",
       :thumb => "100x100>" },
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_KEY']
-    },
     :path => "/:style/:id/:filename"
 
 
